9ca15a29dfacdda562208eb9d3ac7e0fb87f317c
raymondluk1995 <raymondluk1995@outlook.com>
2021-04-26 16:42:35 +1000

    m1

--- chatapp/resources/js/Pages/Chat/container.vue
+++ chatapp/resources/js/Pages/Chat/container.vue
@@ -9,7 +9,8 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    Container
+                    <message-container />
+                    <input-message />
                 </div>
             </div>
         </div>
@@ -18,10 +19,16 @@
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
+    import InputMessage from './inputMessage.vue'
+    import MessageContainer from './messageContainer.vue'
 
     export default {
         components: {
             AppLayout,
+            MessageContainer,
+            InputMessage
         },
     }
 </script>
+
+                MessageContainer
